23637d9c978229d85209b960e46fdfce8e9eba17
Pavel Diatchenko <diatche@gmail.com>
2020-06-10 09:32:15 +1200

    Fixed unit test. - Ignore npm package locks, use yarn lock instead.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-}
+};
