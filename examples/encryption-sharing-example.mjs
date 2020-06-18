07afc9bc5d19dad60b6e7555e830f0e52405173d
Pavel Diatchenko <diatche@gmail.com>
2020-06-18 16:18:32 +1200

    Updated readme and added examples.

---
+++ examples/encryption-sharing-example.mjs
@@ -0,0 +1,40 @@
+import {
+    encrypt,
+    decrypt,
+    SEA,
+} from '../dist/index.mjs';
+
+(async () => {
+    let frodo = await SEA.pair();
+    let gandalf = await SEA.pair();
+    let sauron = await SEA.pair();
+    
+    let enc = await encrypt({ whereami: 'shire' }, {
+        pair: frodo,
+        recipient: gandalf // Or { epub: gandalf.epub }
+    });
+    console.log('enc: ' + JSON.stringify(enc));
+
+    let dec = await decrypt(enc, {
+        pair: gandalf,
+        sender: frodo // Or { epub: frodo.epub }
+    });
+    console.log('dec: ' + JSON.stringify(dec));
+
+    let eye = '';
+    try {
+        eye = await decrypt(enc, {
+            pair: sauron,
+            sender: frodo // Or { epub: frodo.epub }
+        });
+        console.log('eye: ' + JSON.stringify(eye));
+    } catch (err) {
+        console.log('err: ' + err);
+    }
+})();
+
+/**
+ * enc: {"whereami":"SEA{\"ct\":\"HH3f8pMFYgUkkoZ2YPQobVju2UZH\",\"iv\":\"YP4LcGr4aKvIXDbuJ3uU\",\"s\":\"bysXPuyhH/zF\"}"}
+ * dec: {"whereami":"shire"}
+ * err: Error: Could not decrypt
+ */
