0b87b0e998121c00cb0abbb3e57380bc8b380c79
Pavel Diatchenko <diatche@gmail.com>
2020-06-22 16:35:10 +1200

    Renamed file user to GunUser.

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ export * from './const';
 export * from './iterate';
 export * from './temp';
 export * from './types';
-export * from './user';
-export { default as GunUser } from './user';
+export * from './GunUser';
+export { default as GunUser } from './GunUser';
