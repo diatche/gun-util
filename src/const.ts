98c248ff22c144295f8aaa5ded041662130f67cf
Pavel Diatchenko <diatche@gmail.com>
2020-08-18 11:57:37 +1200

    Use peers defined in env.

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,14 @@
 import { IGunConstructorOptions } from 'gun/types/options';
 
+export const GUN_PEERS: string[] = (process.env.GUN_PEERS || process.env.EXPO_GUN_PEERS || '')
+    .split(',')
+    .map(peer => peer.trim())
+    .filter(peer => !!peer);
+
 export const TEST_GUN_OPTIONS: IGunConstructorOptions = {
+    peers: GUN_PEERS,
     radisk: false,
     localStorage: false,
     axe: false,
+    multicast: false,
 };
