edf7f9dfb75668de1d2c11786fea34f74aeb40ab
Pavel Diatchenko <diatche@gmail.com>
2020-08-18 16:14:00 +1200

    Reversed temporary rollup es5 target.

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,6 @@ export default {
             tsconfigOverride: {
                 compilerOptions: {
                     module: 'ESNext',
-                    target: 'es5',
                 }
             }
         }),
